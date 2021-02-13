0508c26e94da73268494c65a60733749c6e8b799
Joe Basquez <jybasquez@yahoo.com>
2021-02-12 19:39:16 -0500

    added in input field for manager

--- index.js
+++ index.js
@@ -2,3 +2,31 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+const list = [
+    {
+        type:'input',
+        message: 'What is the team manager name?',
+        name:'teamManager'
+    },
+    {
+        type: 'input',
+        message: 'What is the managers id number',
+        name: 'managerIdNumber'
+    },
+    {
+        type: 'input',
+        message: 'What is the team managers email address',
+        name: 'managerEmail'
+    },
+    {
+        type: 'input',
+        message: 'What is the team managers github username?',
+        name: 'managerGithub'
+    },
+    {
+        type: 'input',
+        message: 'Managers office number',
+        name: 'managersNum'
+    },
+
+]
