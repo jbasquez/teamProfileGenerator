161ffe6ba6832fd8bad1904b133dc39e27887390
Joe Basquez <jybasquez@yahoo.com>
2021-02-16 20:54:47 -0500

    got questions to ask, fixed list, fixed inquier, and started a few functions

--- index.js
+++ index.js
@@ -1,59 +1,114 @@
 // packges needed
 const fs = require("fs");
 const inquirer = require("inquirer");
-const PORT =3007;
+// const PORT =3007;
 
 //area of questions for managers input
-const list = [
+
+let productionTeam = []
+
+const coreQuestion = [
     {
         type:'input',
-        message: 'What is the team manager name?',
-        name:'teamManager'
+        message: 'What is your name?',
+        name:'name'
     },
     {
-        type: 'input',
-        message: 'What is the managers id number',
-        name: 'managerIdNumber'
+        type:'input',
+        message: 'What is your id number?',
+        name:'id'
     },
     {
-        type: 'input',
-        message: 'What is the team managers email address',
-        name: 'managerEmail'
+        type:'input',
+        message: 'What is your email?',
+        name:'email'
     },
     {
-        type: 'input',
-        message: 'What is the team managers github username?',
-        name: 'managerGithub'
-    },
-    {
-        type: 'input',
-        message: 'Managers office number',
-        name: 'managersNum'
-    },
-
+        type:'list',
+        message: 'What is your role?',
+        name:'role',
+        choices:[ "manager", "intern", "engineer"]
+    }
 ]
-//function to write file
-function renderPage(res, name) {
-    fs.readFile(`${_dirname}${name}.html`, (err, content) => {
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    res.end(content);
-    })
+const managerQuestion =[
+    {
+        type:'input',
+        message: 'What is your office number?',
+        name:'oficeNum'
+    }
+]
+
+// const engineerQuestion
+
+// const interQuestions
+
+function companyPrompt(){
+    console.log("Please fill out teams info:");
+    inquirer
+        .prompt(coreQuestion).then(function(data){
+            console.log(data);
+            if(data.role==="manager"){
+                inquirer
+                    .prompt(managerQuestion).then(function(managerData){
+                        console.log(managerData)
+                    })
+            }
+            //continue conidtional statemnt for two list options
+        })
+
+};
+
+function addAnother(){
+    //this will be another inquier function for the user to add more functions
 }
 
-const server = http.createServer((req, res) => {
-    const path = req.url;
+function createNewManager(data, managerData){
+    // figure out how to create new manager object to push into prodcution team array
+}
 
-    switch(path) {
-        case '/Employee':
-            renderPage(res, 'employee')
-    }
-    switch(path) {
-        case '/Inern':
-            renderPage(res, 'intern')
-    }
-    switch(path) {
-        case '/Manager':
-            renderPage(res, 'manager')
-    }
 
-});
+function createNewIntern(){
+    
+}
+
+function createNewEngineer(){
+    
+}
+
+
+
+companyPrompt();
+
+
+
+
+
+
+
+
+
+//function to write file
+// function renderPage(res, name) {
+//     fs.readFile(`${_dirname}${name}.html`, (err, content) => {
+//     res.writeHead(200,{'Content-Type': 'text/html'});
+//     res.end(content);
+//     })
+// }
+
+// const server = http.createServer((req, res) => {
+//     const path = req.url;
+
+//     switch(path) {
+//         case '/Employee':
+//             renderPage(res, 'employee')
+//     }
+//     switch(path) {
+//         case '/Inern':
+//             renderPage(res, 'intern')
+//     }
+//     switch(path) {
+//         case '/Manager':
+//             renderPage(res, 'manager')
+//     }
+
+// });
