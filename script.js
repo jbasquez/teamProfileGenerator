1a17d3c1a7cc531f9e04dbbc9a4f564cf108b117
Joe Basquez <jybasquez@yahoo.com>
2021-02-18 16:58:09 -0500

    added script.js trying to start html file being made

---
+++ script.js
@@ -0,0 +1,6 @@
+var nametag = document.getElementById("name");
+var idtag = document.getElementById("id");
+var roletag = document.getElementById("role");
+
+//want to replace hello 123 with ${data.name}
+nametag.textContent= "hello 123";
